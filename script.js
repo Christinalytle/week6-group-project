3e23ed446c771e8e962d2c43139e126a97d70909
paulcarlon <paulcarlonlv@gmail.com>
2021-04-23 07:19:22 -0700

    so close now...

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let formTitle = document.querySelector('#formTitle');
+let submitButton = document.querySelector('#submitButton');
+let changeSubmit = document.querySelector('.changeSubmit');
+let isUpdate = false;
 class Movie {
   //Christina
   constructor(title, auditorium, time) {
@@ -9,7 +13,8 @@ class Movie {
 
 class MovieService {
   //Robert
-  static url ='https://crudcrud.com/api/64da7c3225804fa392c2defd75709aad/crudmovies'; 
+  static url =
+    'https://crudcrud.com/api/b448e9c1cb564baab8a396f34f2e85bf/crudmovies';
 
   static getAllMovies() {
     return $.get(this.url);
@@ -26,18 +31,16 @@ class MovieService {
       crossDomain: true,
       data: JSON.stringify(movie),
       dataType: 'json',
-      contentType: 'application/json'
+      contentType: 'application/json',
     });
   }
-    
 
-  static updateMovie(movie) {
+  static updateMovie(id) {
     return $.ajax({
-      url: this.url + `/${movie._id}`,
-      dataType: 'json',
-      data: JSON.stringify(movie),
-      contentType: 'application/json',
+      url: this.url + `/${id}`,
       type: 'PUT',
+      dataType: 'json',
+      data: movie,
     });
   }
 
@@ -65,16 +68,34 @@ class DOMManager {
       .then((movies) => this.render(movies));
   }
 
-  static getAllMovies() {
-    MovieService.getAllMovies().then((movies) => this.render(movies));
-  }
-
-  static createMovie(title, auditorium, time) {
-    MovieService.createMovie(new Movie(title, auditorium, time))
-      .then(() => {
-        return MovieService.getAllMovies();
-      })
-      .then((movies) => this.render(movies));
+  static updateMovie(id) {
+    isUpdate = true;
+    formTitle.innerText = 'Edit Movie:';
+    submitButton.innerText = 'Update';
+    changeSubmit.id = 'updateSubmit';
+    console.log(changeSubmit);
+    MovieService.getAllMovies().then((movies) => {
+      movies.forEach((movie) => {
+        if (movie._id == id) {
+          document.querySelector('#movieTitle').value = movie.title;
+          document.querySelector('#auditorium').value = movie.auditorium;
+          document.querySelector('#time').value = movie.time;
+          $('#submitButton').on('click', submitUpdate);
+          function submitUpdate(title, auditorium, time) {
+            console.log(title, auditorium, time);
+          }
+        }
+      });
+      // MovieService.updateMovie(id);
+    });
+    // formTitle.innerText = 'Enter New Movie:';
+    // submitButton.innerText = 'Submit';
+    $('#updateSubmit').on('click', (e) => {
+      console.log(e.target.id);
+      let title = $('#movieTitle').val();
+      let auditorium = $('#auditorium').val();
+      let time = $('#time').val();
+    });
   }
 
   static deleteMovie(id) {
@@ -86,15 +107,15 @@ class DOMManager {
   }
 
   static render(movies) {
-    this.movies = movies; 
+    this.movies = movies;
     $('#moviesPlaying').empty();
     movies.forEach((movie) => {
       let newChild = `<div class="col-sm-3">
       <div class="card" id="newMovie" id="${movie._id} style="width: 18rem;">
-        <h5 class="card-header">${movie.title}</h5>
+        <h5 class="card-header" id="inputTitle">${movie.title}</h5>
         <div class="card-body">
-          <h5 class="card-title">${movie.auditorium}</h5>
-          <p class="card-text">${movie.time}</p>
+          <h5 class="card-title" id="inputAuditorium">${movie.auditorium}</h5>
+          <p class="card-text" id="inputTime">${movie.time}</p>
           <button type="button" class="btn btn-primary delete" onclick="DOMManager.deleteMovie('${movie._id}')">Delete</button>
           <button type="button" class="btn btn-primary delete" onclick="DOMManager.updateMovie('${movie._id}')">Update</button>
         </div>
@@ -105,12 +126,15 @@ class DOMManager {
   }
 }
 
-
 $('#submitButton').on('click', () => {
   let title = $('#movieTitle').val();
   let auditorium = $('#auditorium').val();
   let time = $('#time').val();
-  DOMManager.createMovie(title, auditorium, time); 
+
+  DOMManager.createMovie(title, auditorium, time);
+  title = '';
+  auditorium = '';
+  time = '';
 });
 
 DOMManager.getAllMovies();
